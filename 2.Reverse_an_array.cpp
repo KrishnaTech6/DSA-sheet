9b9e4e93aedbde9fb8db1a77a4876772d18a013c
krishnaTech6 <krishna060702@gmail.com>
2025-01-05 18:00:10 +0530

    2. Reversing a string

---
+++ 2.Reverse_an_array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void swap(int &a , int &b){
+    int temp = a ; 
+    a = b; 
+    b= temp; 
+}
+ 
+int main()
+{
+    int arr[]={2,3,4,5,6}; 
+    int n= 5, i= 0 ; 
+    while(i<n/2){
+        swap(*(arr+i) , *(arr + n-i-1)); 
+        i++; 
+    }
+    
+    for(int element: arr){
+        cout<< element<<" " ;
+    }
+ 
+    return 0;
+}
