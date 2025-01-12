a3a0eb1bd155b74d4420994e11db755f45817d31
krishnaTech6 <krishna060702@gmail.com>
2025-01-12 22:55:57 +0530

    4 using hash map

--- 4.contains_duplicate.cpp
+++ 4.contains_duplicate.cpp
@@ -1,21 +1,37 @@
 #include<iostream>
 #include<vector>
+#include <unordered_map>
 
 using namespace std;
 
+//O(nlogn) approach
+
+// bool containsDuplicate(vector<int>& nums) {
+//         sort(nums.begin(), nums.end()); 
+//         for(int i =0 ; i< nums.size()-1; i++ ){
+//             if(nums[i]==nums[i+1]){
+//                 return true;  
+//             }
+//         }
+//         return false; 
+//     }
+
+//hashmap approach O(1)
 bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end()); 
-        for(int i =0 ; i< nums.size()-1; i++ ){
-            if(nums[i]==nums[i+1]){
-                return true;  
-            }
+        unordered_map<int, int> seen;
+        for (int num : nums) {
+            if (seen[num] >= 1)
+                return true;
+            seen[num]++;
         }
-        return false; 
+        return false;
     }
+
  
 int main()
 {
-    vector<int> arr={1,2,4,5} ;
+    vector<int> arr = {1,2,3,4,1};
+
     cout<<"Is Duplicate: "<<containsDuplicate(arr)<<endl; 
  
     return 0;
